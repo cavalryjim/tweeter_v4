22140e495201b80fdcc766131ee1ff263368872d
James Davis <james.davisphd@gmail.com>
2020-02-02 15:47:22 -0600

    configure for heroku2

--- tweeter_app/settings.py
+++ tweeter_app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku # <- this line goes near the top of the file
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals()) # <- this line should be a the end of the file
